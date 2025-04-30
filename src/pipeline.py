cf0f206f0a3b0792c9826d70311f7f481dc7405b
mark-alexander.mueller@telekom.de <mark-alexander.mueller@telekom.de>
2025-04-30 10:33:58 +0200

    First commit

--- src/pipeline.py
+++ src/pipeline.py
@@ -1,5 +1,11 @@
 from quixstreams import Application
 
 app = Application(
-    broker_address="publickafka.quix.io:9092"
-)
+    broker_address="publickafka.quix.io:9092",
+    loglevel="DEBUG"
+    )
+
+with app.get_producer() as producer:
+    producer.produce(
+        topic = "test"
+    )
