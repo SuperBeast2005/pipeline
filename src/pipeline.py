c2f0356fbdaba774ba95820981190ab623b23dd3
SuperBeast2005 <muellerma2023@gmail.com>
2025-05-01 10:46:51 +0200

    loggin

--- src/pipeline.py
+++ src/pipeline.py
@@ -1,4 +1,5 @@
 from quixstreams import Application
+import logging
 
 app = Application(
     broker_address="publickafka.quix.io:9092",
@@ -9,3 +10,10 @@ with app.get_producer() as producer:
     producer.produce(
         topic = "test"
     )
+    
+def main():
+    logging.info("STARTING")
+    
+if __name__=="__main__":
+    logging.basicConfig(level="DEBUG")
+    main()
